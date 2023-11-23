5673d05e22e4d7115c8c7d7621655bec8c6910c7
Kalinina Oksana <ksan4ik92@gmail.com>
2023-11-23 22:52:27 +0300

    fix: style for android

--- src/components/utils/StopGame/StopGame.tsx
+++ src/components/utils/StopGame/StopGame.tsx
@@ -1,5 +1,5 @@
 //for Task 1
-
+import { useState } from "react";
 interface IProps {
     funcBtn: () => void;
     screen: number;
@@ -7,9 +7,15 @@ interface IProps {
 
 function StopGame(props: IProps) {
     const { funcBtn, screen } = props;
+    const [touchClick, setTouchClick] = useState(true);
+    const click = () => {
+        if (!touchClick) return;
+        funcBtn();
+        setTouchClick(false);
+    }
     return (
         <>
-            <div className={"stop-game " + (((screen < 3) || (screen === 20) || (screen === 30) || (screen === 40)) ? "" : "stop-task1Icon")} onClick={funcBtn}></div>
+            <div className={"stop-game " + (((screen < 3) || (screen === 20) || (screen === 30) || (screen === 40)) ? "" : "stop-task1Icon")} onClick={click} onTouchStart={click}></div>
         </>
     )
 }
