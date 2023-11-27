6f78c1381b4d9246f2402dd7c4a8fad5e57c7524
Kalinina Oksana <ksan4ik92@gmail.com>
2023-11-28 00:13:44 +0300

    fix: resize

--- src/resize.ts
+++ src/resize.ts
@@ -1,10 +1,10 @@
 export const resize = () => {
-    const appHeight = () => {
-        const doc = document.documentElement;
-        console.log(doc);
+    const vh = window.innerHeight * 0.01;
+    console.log(vh);
 
-        doc.style.setProperty('--app-height', '${window.innerHeight}px')
-    }
-    window.addEventListener('resize', appHeight)
-    appHeight()
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    window.addEventListener('resize', () => {
+        const vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
 }
